1c88f2e6a7cb7384d848c9b244b28d700fb1aa94
Matt Norton <matt@carrotmanmatt.com>
2024-05-16 18:22:28 +0100

    Fix failing test

--- tests/test_console.py
+++ tests/test_console.py
@@ -14,7 +14,7 @@ from ccft_pymarkdown import console
 class TestConsoleRun:
     """Test case to unit-test the `run` function."""
 
-    USAGE_MESSAGE: Final[str] = "usage: ccft-pymarkdown [-h] {remove,restore,scan-all}"
+    USAGE_MESSAGE: Final[str] = "usage: ccft-pymarkdown [-h] {clean,restore,scan-all}"
 
     def test_error_when_no_args(self, capsys: CaptureFixture[str]) -> None:
         EXPECTED_ERROR_MESSAGE: Final[str] = (
